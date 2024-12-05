d7b26915b8f4a8ea4ab003ce8f4af46b5653926e
ShenYuuu <thegaoshuai@163.com>
2024-12-05 09:02:29 +0800

    1

--- original_script.py
+++ original_script.py
@@ -1,3 +1,4 @@
+
 from typing import Dict, Tuple
 from tqdm import tqdm
 import torch
@@ -10,11 +11,6 @@ from torchvision.utils import save_image, make_grid
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, PillowWriter
 import numpy as np
-import os
-from PIL import Image
-import tarfile
-import json
-import io
 
 class ResidualConvBlock(nn.Module):
     def __init__(
@@ -105,7 +101,7 @@ class EmbedFC(nn.Module):
 
 
 class ContextUnet(nn.Module):
-    def __init__(self, in_channels=3, n_feat=256, n_classes=1):
+    def __init__(self, in_channels, n_feat = 256, n_classes=10):
         super(ContextUnet, self).__init__()
 
         self.in_channels = in_channels
@@ -117,7 +113,7 @@ class ContextUnet(nn.Module):
         self.down1 = UnetDown(n_feat, n_feat)
         self.down2 = UnetDown(n_feat, 2 * n_feat)
 
-        self.to_vec = nn.Sequential(nn.AvgPool2d(32), nn.GELU())
+        self.to_vec = nn.Sequential(nn.AvgPool2d(7), nn.GELU())
 
         self.timeembed1 = EmbedFC(1, 2*n_feat)
         self.timeembed2 = EmbedFC(1, 1*n_feat)
@@ -125,7 +121,8 @@ class ContextUnet(nn.Module):
         self.contextembed2 = EmbedFC(n_classes, 1*n_feat)
 
         self.up0 = nn.Sequential(
-            nn.ConvTranspose2d(2 * n_feat, 2 * n_feat, 32, 32),
+            # nn.ConvTranspose2d(6 * n_feat, 2 * n_feat, 7, 7), # when concat temb and cemb end up w 6*n_feat
+            nn.ConvTranspose2d(2 * n_feat, 2 * n_feat, 7, 7), # otherwise just have 2*n_feat
             nn.GroupNorm(8, 2 * n_feat),
             nn.ReLU(),
         )
@@ -148,9 +145,11 @@ class ContextUnet(nn.Module):
         down2 = self.down2(down1)
         hiddenvec = self.to_vec(down2)
 
+        # convert context to one hot embedding
+        c = nn.functional.one_hot(c, num_classes=self.n_classes).type(torch.float)
         
         # mask out context if context_mask == 1
-        context_mask = context_mask.view(-1,1)
+        context_mask = context_mask[:, None]
         context_mask = context_mask.repeat(1,self.n_classes)
         context_mask = (-1*(1-context_mask)) # need to flip 0 <-> 1
         c = c * context_mask
@@ -231,240 +230,152 @@ class DDPM(nn.Module):
         # We should predict the "error term" from this x_t. Loss is what we return.
 
         # dropout context with some probability
-        context_mask = torch.bernoulli(torch.zeros_like(c[:,0])+self.drop_prob).to(self.device)
-
+        context_mask = torch.bernoulli(torch.zeros_like(c)+self.drop_prob).to(self.device)
         
         # return MSE between added noise, and our predicted noise
         return self.loss_mse(noise, self.nn_model(x_t, c, _ts / self.n_T, context_mask))
 
-    def sample(self, n_sample, size, device, target_labels=None, guide_w=0.0):
-        """
-        target_labels: [n_sample, n_classes] 指定要生成的目标类别的one-hot编码
-        """
-        x_i = torch.randn(n_sample, *size).to(device)
-        
-        if target_labels is None:
-            # 如果没有指定标签，随机生成一些标签组合
-            target_labels = torch.randint(0, 2, (n_sample, self.n_classes)).float().to(device)
-        
-        context_mask = torch.zeros(n_sample).to(device)
-        
-        # 双倍批次大小
-        x_i_double = torch.cat([x_i, x_i])  # [2*n_sample, C, H, W]
-        c_i_double = torch.cat([target_labels, target_labels])  # [2*n_sample, n_classes]
-        context_mask_double = torch.cat([context_mask, torch.ones_like(context_mask)])  # [2*n_sample]
-        
-        x_i_store = []
+    def sample(self, n_sample, size, device, guide_w = 0.0):
+        # we follow the guidance sampling scheme described in 'Classifier-Free Diffusion Guidance'
+        # to make the fwd passes efficient, we concat two versions of the dataset,
+        # one with context_mask=0 and the other context_mask=1
+        # we then mix the outputs with the guidance scale, w
+        # where w>0 means more guidance
+
+        x_i = torch.randn(n_sample, *size).to(device)  # x_T ~ N(0, 1), sample initial noise
+        c_i = torch.arange(0,10).to(device) # context for us just cycles throught the mnist labels
+        c_i = c_i.repeat(int(n_sample/c_i.shape[0]))
+
+        # don't drop context at test time
+        context_mask = torch.zeros_like(c_i).to(device)
+
+        # double the batch
+        c_i = c_i.repeat(2)
+        context_mask = context_mask.repeat(2)
+        context_mask[n_sample:] = 1. # makes second half of batch context free
+
+        x_i_store = [] # keep track of generated steps in case want to plot something 
+        print()
         for i in range(self.n_T, 0, -1):
             print(f'sampling timestep {i}',end='\r')
             t_is = torch.tensor([i / self.n_T]).to(device)
-            t_is_double = t_is.repeat(2*n_sample)
-            
-            # 生成噪声
-            z = torch.randn_like(x_i_double) if i > 1 else 0
-            
-            # 确保context_mask维度正确
-            current_context_mask = context_mask_double.view(-1, 1)
-            
-            # 预测噪声
-            eps = self.nn_model(x_i_double, c_i_double, t_is_double, current_context_mask)
-            eps1, eps2 = eps.chunk(2)  # 将预测分成两半
-            eps = (1 + guide_w) * eps1 - guide_w * eps2
-            
-            # 更新采样
+            t_is = t_is.repeat(n_sample,1,1,1)
+
+            # double batch
+            x_i = x_i.repeat(2,1,1,1)
+            t_is = t_is.repeat(2,1,1,1)
+
+            z = torch.randn(n_sample, *size).to(device) if i > 1 else 0
+
+            # split predictions and compute weighting
+            eps = self.nn_model(x_i, c_i, t_is, context_mask)
+            eps1 = eps[:n_sample]
+            eps2 = eps[n_sample:]
+            eps = (1+guide_w)*eps1 - guide_w*eps2
+            x_i = x_i[:n_sample]
             x_i = (
                 self.oneover_sqrta[i] * (x_i - eps * self.mab_over_sqrtmab[i])
-                + self.sqrt_beta_t[i] * (z[:n_sample] if isinstance(z, torch.Tensor) else z)
+                + self.sqrt_beta_t[i] * z
             )
-            
-            # 更新双倍批次
-            x_i_double = torch.cat([x_i, x_i])
-            
-            if i % 20 == 0 or i == self.n_T or i < 8:
+            if i%20==0 or i==self.n_T or i<8:
                 x_i_store.append(x_i.detach().cpu().numpy())
         
         x_i_store = np.array(x_i_store)
         return x_i, x_i_store
 
 
-class CustomDataset(torch.utils.data.Dataset):
-    def __init__(self, tar_path, transform=None):
-        self.tar_path = tar_path
-        self.transform = transform
-        self.tar = tarfile.open(tar_path, 'r')
-        
-        # 修改：指定train目录下的文件
-        self.img_prefix = "train/img/"
-        self.ann_prefix = "train/ann/"
-        
-        # 加载标注数据和图像列表
-        self.annotations, self.classes = self._load_annotations()
-        self.class_to_idx = {cls_name: i for i, cls_name in enumerate(self.classes)}
-        print(f"Found {len(self.classes)} classes: {self.classes}")
-        
-    def _load_annotations(self):
-        annotations = {}
-        classes = set()
-        
-        # 遍历tar文件中的标注文件
-        for member in self.tar.getmembers():
-            if member.name.startswith(self.ann_prefix) and member.name.endswith('.json'):
-                f = self.tar.extractfile(member)
-                if f is not None:
-                    annotation_data = json.load(f)
-                    
-                    # 获取对应的图像文件名
-                    json_name = os.path.basename(member.name)
-                    image_name = self.img_prefix + json_name.replace('.json', '')
-                    
-                    # 收集该图像中的所有对象类别
-                    object_classes = []
-                    for obj in annotation_data.get('objects', []):
-                        class_title = obj.get('classTitle')
-                        if class_title:
-                            object_classes.append(class_title)
-                            classes.add(class_title)
-                    
-                    if object_classes:  # 只添加有标注的图像
-                        annotations[image_name] = object_classes
-        
-        return annotations, sorted(list(classes))
-    
-    def __len__(self):
-        return len(self.annotations)
-    
-    def __getitem__(self, idx):
-        img_name = list(self.annotations.keys())[idx]
-        
-        # 从tar文件中读取图像
-        try:
-            img_file = self.tar.extractfile(img_name)
-            if img_file is None:
-                raise ValueError(f"Cannot find image {img_name} in tar file")
-            
-            # 读取图像数据
-            img_data = img_file.read()
-            image = Image.open(io.BytesIO(img_data)).convert('RGB')
-            
-            # 获取标签并创建one-hot编码
-            labels = self.annotations[img_name]
-            label_tensor = torch.zeros(len(self.classes))
-            for label in labels:
-                label_tensor[self.class_to_idx[label]] = 1
-            
-            if self.transform:
-                image = self.transform(image)
-                
-            return image, label_tensor
-            
-        except Exception as e:
-            print(f"Error loading image {img_name}: {str(e)}")
-            # 返回数据集中的下一个样本
-            return self.__getitem__((idx + 1) % len(self))
-    
-    def __del__(self):
-        if hasattr(self, 'tar'):
-            self.tar.close()
+def train_mnist():
 
-
-def train_custom_dataset():
-    # 修改训练参数
-    n_epoch = 100
-    batch_size = 4
-    n_T = 400
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    # hardcoding these here
+    n_epoch = 20
+    batch_size = 256
+    n_T = 400 # 500
+    device = "cuda:0"
+    n_classes = 10
+    n_feat = 128 # 128 ok, 256 better (but slower)
     lrate = 1e-4
-    save_dir = './data/'
-    ws_test = [0.0, 0.5, 2.0]
-    save_model = True
-    
-    # 确保保存目录存在
-    os.makedirs(save_dir, exist_ok=True)
-    
-    # 数据预处理
-    tf = transforms.Compose([
-        transforms.Resize((256, 256)),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-    
-    # 加载tar文件中的数据集
-    dataset = CustomDataset("/home/usr/ciwh01/road-damage-detector-DatasetNinja.tar", transform=tf)
-    
-    # 创建数据加载器
-    dataloader = DataLoader(
-        dataset, 
-        batch_size=batch_size, 
-        shuffle=True, 
-        num_workers=5,
-        pin_memory=True
-    )
-    
-    # 创建模型实例
-    ddpm = DDPM(
-        nn_model=ContextUnet(in_channels=3, n_feat=256, n_classes=len(dataset.classes)),
-        betas=(1e-4, 0.02),
-        n_T=n_T,
-        device=device,
-        drop_prob=0.1
-    )
+    save_model = False
+    save_dir = './data/diffusion_outputs10/'
+    ws_test = [0.0, 0.5, 2.0] # strength of generative guidance
+
+    ddpm = DDPM(nn_model=ContextUnet(in_channels=1, n_feat=n_feat, n_classes=n_classes), betas=(1e-4, 0.02), n_T=n_T, device=device, drop_prob=0.1)
     ddpm.to(device)
-    
-    # 优化器
+
+    # optionally load a model
+    # ddpm.load_state_dict(torch.load("./data/diffusion_outputs/ddpm_unet01_mnist_9.pth"))
+
+    tf = transforms.Compose([transforms.ToTensor()]) # mnist is already normalised 0 to 1
+
+    dataset = MNIST("./data", train=True, download=True, transform=tf)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=5)
     optim = torch.optim.Adam(ddpm.parameters(), lr=lrate)
-    
-    # 训练循环
+
     for ep in range(n_epoch):
-        print(f'Epoch {ep}')
+        print(f'epoch {ep}')
         ddpm.train()
-        
-        # 学习率衰减
-        optim.param_groups[0]['lr'] = lrate * (1 - ep/n_epoch)
-        
+
+        # linear lrate decay
+        optim.param_groups[0]['lr'] = lrate*(1-ep/n_epoch)
+
         pbar = tqdm(dataloader)
         loss_ema = None
-        
         for x, c in pbar:
             optim.zero_grad()
             x = x.to(device)
             c = c.to(device)
             loss = ddpm(x, c)
             loss.backward()
-            
             if loss_ema is None:
                 loss_ema = loss.item()
             else:
                 loss_ema = 0.95 * loss_ema + 0.05 * loss.item()
-            
             pbar.set_description(f"loss: {loss_ema:.4f}")
             optim.step()
         
-        # 每个epoch保存样本
-        if ep % 5 == 0 or ep == n_epoch-1:
-            ddpm.eval()
-            with torch.no_grad():
-                for w_i, w in enumerate(ws_test):
-                    # 为每个类别生成样本
-                    for class_idx, class_name in enumerate(dataset.classes):
-                        n_sample = 4
-                        # 创建目标标签
-                        target_labels = torch.zeros(n_sample, len(dataset.classes))
-                        target_labels[:, class_idx] = 1  # 设置目标类别
-                        target_labels = target_labels.to(device)
-                        
-                        # 生成样本
-                        x_gen, _ = ddpm.sample(n_sample, (3, 256, 256), device, 
-                                             target_labels=target_labels, guide_w=w)
-                        
-                        # 保存生成的图片
-                        grid = make_grid(x_gen * 0.5 + 0.5, nrow=2)
-                        save_image(grid, 
-                                 f"{save_dir}/ep{ep}_w{w}_{class_name}.png")
-        
-        # 保存模型
-        if save_model and ep == n_epoch-1:
-            torch.save(ddpm.state_dict(), f"{save_dir}/model_ep{ep}.pth")
-            print(f'Saved model at {save_dir}/model_ep{ep}.pth')
+        # for eval, save an image of currently generated samples (top rows)
+        # followed by real images (bottom rows)
+        ddpm.eval()
+        with torch.no_grad():
+            n_sample = 4*n_classes
+            for w_i, w in enumerate(ws_test):
+                x_gen, x_gen_store = ddpm.sample(n_sample, (1, 28, 28), device, guide_w=w)
+
+                # append some real images at bottom, order by class also
+                x_real = torch.Tensor(x_gen.shape).to(device)
+                for k in range(n_classes):
+                    for j in range(int(n_sample/n_classes)):
+                        try: 
+                            idx = torch.squeeze((c == k).nonzero())[j]
+                        except:
+                            idx = 0
+                        x_real[k+(j*n_classes)] = x[idx]
+
+                x_all = torch.cat([x_gen, x_real])
+                grid = make_grid(x_all*-1 + 1, nrow=10)
+                save_image(grid, save_dir + f"image_ep{ep}_w{w}.png")
+                print('saved image at ' + save_dir + f"image_ep{ep}_w{w}.png")
+
+                if ep%5==0 or ep == int(n_epoch-1):
+                    # create gif of images evolving over time, based on x_gen_store
+                    fig, axs = plt.subplots(nrows=int(n_sample/n_classes), ncols=n_classes,sharex=True,sharey=True,figsize=(8,3))
+                    def animate_diff(i, x_gen_store):
+                        print(f'gif animating frame {i} of {x_gen_store.shape[0]}', end='\r')
+                        plots = []
+                        for row in range(int(n_sample/n_classes)):
+                            for col in range(n_classes):
+                                axs[row, col].clear()
+                                axs[row, col].set_xticks([])
+                                axs[row, col].set_yticks([])
+                                # plots.append(axs[row, col].imshow(x_gen_store[i,(row*n_classes)+col,0],cmap='gray'))
+                                plots.append(axs[row, col].imshow(-x_gen_store[i,(row*n_classes)+col,0],cmap='gray',vmin=(-x_gen_store[i]).min(), vmax=(-x_gen_store[i]).max()))
+                        return plots
+                    ani = FuncAnimation(fig, animate_diff, fargs=[x_gen_store],  interval=200, blit=False, repeat=True, frames=x_gen_store.shape[0])    
+                    ani.save(save_dir + f"gif_ep{ep}_w{w}.gif", dpi=100, writer=PillowWriter(fps=5))
+                    print('saved image at ' + save_dir + f"gif_ep{ep}_w{w}.gif")
+        # optionally save model
+        if save_model and ep == int(n_epoch-1):
+            torch.save(ddpm.state_dict(), save_dir + f"model_{ep}.pth")
+            print('saved model at ' + save_dir + f"model_{ep}.pth")
 
 if __name__ == "__main__":
-    train_custom_dataset()
+    train_mnist()
